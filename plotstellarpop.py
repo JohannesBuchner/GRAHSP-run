950811cb1a2aa53dc086066ceadbb9a2e0f7bf2c
Johannes Buchner <johannes.buchner.acad@gmx.com>
2022-11-01 19:20:32 +0100

    add helpful plotting of SEDs

---
+++ plotstellarpop.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+from pcigale import creation_modules
+from pcigale.sed import SED
+import matplotlib.pyplot as plt
+
+
+sed = SED()
+sfh = creation_modules.get_module(
+    'sfhdelayed', tau_main=1000, age_main=1000, sfr_A=1, normalise="True")
+sfh.process(sed)
+
+bc03 = creation_modules.get_module(
+    'bc03', imf=0, metallicity=0.02, separation_age=10)
+bc03.process(sed)
+
+m = np.logical_and(sed.wavelength_grid > 50, sed.wavelength_grid < 1e5)
+plt.plot(sed.wavelength_grid[m], sed.luminosities.sum(axis=0)[m], label='BC03', lw=0.3)
+
+sed = SED()
+sfh.process(sed)
+
+m2005 = creation_modules.get_module(
+    'm2005', imf=0, metallicity=0.02, separation_age=10)
+m2005.process(sed)
+
+m = np.logical_and(sed.wavelength_grid > 50, sed.wavelength_grid < 1e5)
+plt.plot(sed.wavelength_grid[m], sed.luminosities.sum(axis=0)[m], label='Maraston+2005', lw=0.3)
+
+plt.xlim(50, 1e5)
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel("Wavelength [nm]")
+plt.ylabel("Luminosity [W]")
+plt.legend(title='%d Myr ago, $\\tau$=%d Myr' % (sfh.age_main, sfh.tau_main))
+plt.savefig('plotstellarpop.pdf')
+plt.close()
